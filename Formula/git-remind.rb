e4c2559a28dc8819798983cd511733852cac4950
Javier Juan Oltra <javijuol@gmail.com>
2022-03-19 14:13:44 +0100

    Update git-remind.rb

--- Formula/git-remind.rb
+++ Formula/git-remind.rb
@@ -3,7 +3,7 @@ class GitRemind < Formula
   homepage "https://github.com/javijuol/git-remind/"
   url "https://github.com/javijuol/git-remind/releases/download/v1.1.3/git-remind_1.1.3_Darwin_x86_64.tar.gz"
   version "1.1.3"
-  sha256 "cfc6a4f43d09a85d86584115340456ca314a29c31acb8d24940cebb0a0474c44"
+  sha256 "51b7577e48ed7b26b0bb8cb1857cf2164563aea0fb41f80a509305d6eaa909e9"
   
   depends_on "git"
 
