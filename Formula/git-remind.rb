ef899f017d369378359cbac344f7aebabb82e4c8
Javier Juan Oltra <javijuol@gmail.com>
2022-03-19 14:07:42 +0100

    Update git-remind.rb

--- Formula/git-remind.rb
+++ Formula/git-remind.rb
@@ -1,9 +1,9 @@
 class GitRemind < Formula
   desc "Never forget to git commit and push."
   homepage "https://github.com/javijuol/git-remind/"
-  url "https://github.com/javijuol/git-remind/releases/download/v1.1.2/git-remind_1.1.2_Darwin_x86_64.tar.gz"
-  version "1.1.2"
-  sha256 "9f2163c3f15b07d8f7c394b39e5dfd29545796e5a5c3ff30a1c823b16823d3f6"
+  url "https://github.com/javijuol/git-remind/releases/download/v1.1.3/git-remind_1.1.3_Darwin_x86_64.tar.gz"
+  version "1.1.3"
+  sha256 "cfc6a4f43d09a85d86584115340456ca314a29c31acb8d24940cebb0a0474c44"
   
   depends_on "git"
 
